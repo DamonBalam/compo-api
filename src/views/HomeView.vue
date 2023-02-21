85d4b9fba8999d994199c5dd01e6b4c3d59d27d7
Arturo Saldivar <damonbalam@gmail.com>
2023-02-21 13:19:56 -0500

    ref vs reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Reactive vs Ref</h1>
+
+    <div>{{ ironman }}</div>
+    <div>{{ hulk }}</div>
+
+    <button @click="changeIronman">Change IronMan</button>
+    <button @click="changeHulk">Change Hulk</button>
+
+    <div>Ironman: {{ ironmanIsAlive }}</div>
+    <div>Hulk: {{ hulkIsAlive }}</div>
+    <button @click="changeStatus">Change</button>
   </div>
 </template>
 
 <script>
+import { reactive, ref } from "vue";
 export default {
   name: "HomeView",
+
+  setup() {
+    const ironman = ref({ name: "Tony", age: 50 });
+    const hulk = reactive({ name: "Bruce", age: 50 });
+
+    const ironmanIsAlive = reactive(false);
+    const hulkIsAlive = ref(true);
+
+    const changeStatus = () => {
+      hulkIsAlive.value = !hulkIsAlive.value;
+    };
+
+    return {
+      ironman,
+      hulk,
+      ironmanIsAlive,
+      hulkIsAlive,
+
+      changeStatus,
+      changeIronman: () => {
+        ironman.value.name = "Tony Stark";
+        ironman.value.age = 55;
+      },
+      changeHulk: () => {
+        hulk.name = "Bruce Banner";
+        hulk.age = 55;
+      },
+    };
+  },
 };
 </script>
