ec3d2a27d7353271eb1262ee2eec2124d383e522
Arturo Saldivar <damonbalam@gmail.com>
2023-02-23 21:14:10 -0500

    Avances de composables

---
+++ src/views/SearchPokemon.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Buscar pokemon:</h1>
+  <form action="">
+    <input type="number" placeholder="Número de pokémon" autofocus>
+    <br>
+    <span>Presione enter para continuar</span>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,16 +1,52 @@
 <template>
-    <h2>Espere por favor...</h2>
+  <h2 v-if="isLoading">Espere por favor...</h2>
+  <h2 v-else>Usuarios</h2>
+  <h5 v-if="errorMessage">{{ errorMessage }}</h5>
+
+  <div v-if="users.length > 0">
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <h4>{{ user.first_name }} {{ user.last_name }}</h4>
+        <h6>{{ user.email }}</h6>
+      </li>
+    </ul>
+  </div>
+
+  <button @click="prevPage">Atrás</button>
+  <button @click="nextPage">Adelante</button>
+  <div>
+    <span>Pagina {{ currentPage }}</span>
+  </div>
 </template>
 
 <script>
+import useUser from "@/composables/useUser";
 export default {
+  setup() {
+    const { users, isLoading, currentPage, errorMessage, prevPage, nextPage } =
+      useUser();
 
-    setup() {
-
-    }
-}
+    return {
+      users,
+      isLoading,
+      currentPage,
+      errorMessage,
+      prevPage,
+      nextPage,
+    };
+  },
+};
 </script>
 
 <style scoped>
+h2 {
+  text-align: center;
+  width: 100%;
+}
 
+div {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
 </style>
